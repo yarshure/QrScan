95fe5e3f19c2e1334c606d37b43469a701122cc7
abigt <abigt@gmail.com>
2016-12-08 11:32:33 +0800

    Initial Commit

---
+++ testReadScan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  testReadScan
+//
+//  Created by 孔祥波 on 08/12/2016.
+//  Copyright © 2016 Kong XiangBo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ testReadScan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  testReadScan
+//
+//  Created by 孔祥波 on 08/12/2016.
+//  Copyright © 2016 Kong XiangBo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
